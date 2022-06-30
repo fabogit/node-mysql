25830bc4b1604dd6a9cb386ad888b6cca30d65f1
fabogit <fabo.dev.email@gmail.com>
2022-06-30 21:20:50 +0200

    fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 node-mysql
 
-- createfile named `.env` and paste:
+- in dir folder create a file named `.env` and paste:
 
 ```
 PORT=3000
@@ -10,6 +10,8 @@ DB_USER="root"
 DB_PASSWORD="mysql"
 DB_NAME="todo"
 ```
+
+to run: `npm install && npm start`
 
 - automate testing crud
 - add f/end
--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(tasksRoutes);
 
 sequelize.authenticate()
 	.then(() => {
-		console.log('Connection has been established successfully.');
+		console.log('SEQUELIZE - Connection has been established successfully.');
 	})
 	.catch(err => {
-		console.error('Unable to connect to the database:', err);
+		console.error('SEQUELIZE - Unable to connect to the database:', err);
 	});
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`NODEJS - Listening on port ${port}`));
